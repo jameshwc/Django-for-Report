e79a9da2fb0b4af60acc599c54ccef10e71b648f
jameshwc <b07902001@csie.ntu.edu.tw>
2019-10-27 21:54:15 +0800

    update gitignore

--- Report/views.py
+++ Report/views.py
@@ -22,12 +22,13 @@ def IP_log(request,url=None):
     data.save()
 
 def handler404(request, exception):
+    IP_log(request)
     response = render(request, '404.html')
     response.status_code = 404
     return response
 
-
 def handler500(request):
+    IP_log(request)
     response = render(request, '404.html')
     response.status_code = 500
     return response
